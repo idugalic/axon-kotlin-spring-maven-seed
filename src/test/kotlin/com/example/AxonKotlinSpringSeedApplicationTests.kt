fdc67d3cecdb4259cfcb5814952574690459f64a
Ivan Dugalic <ivan.dugalic@axoniq.io>
2018-12-12 20:27:48 +0100

    Renaming

--- src/test/kotlin/com/example/AxonKotlinSpringSeedApplicationTests.kt
+++ src/test/kotlin/com/example/AxonKotlinSpringSeedApplicationTests.kt
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class AxonKotlinSpringMavenSeedApplicationTests {
+class AxonKotlinSpringSeedApplicationTests {
 
     @Test
     fun contextLoads() {
