a01ca8f29dc4fdfcc1fe7c89cd3d7c22d25594fe
Ivan Dugalic <ivan.dugalic@axoniq.io>
2018-12-12 20:19:26 +0100

    Update AxonKotlinSpringSeedApplication.kt

--- src/main/kotlin/com/example/AxonKotlinSpringSeedApplication.kt
+++ src/main/kotlin/com/example/AxonKotlinSpringSeedApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DemoApplication
+class AxonKotlinSpringSeedApplication
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
