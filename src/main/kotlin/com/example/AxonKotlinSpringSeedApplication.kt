6071dd3c3c734009ebe66067d405c0b45ed1617b
Ivan Dugalic <ivan.dugalic@axoniq.io>
2018-12-12 20:24:13 +0100

    Renaming

--- src/main/kotlin/com/example/AxonKotlinSpringSeedApplication.kt
+++ src/main/kotlin/com/example/AxonKotlinSpringSeedApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class AxonKotlinSpringSeedApplication
 
 fun main(args: Array<String>) {
-    runApplication<DemoApplication>(*args)
+    runApplication<AxonKotlinSpringSeedApplication>(*args)
 }
